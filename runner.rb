5690b6acfec013e98967496e283dd4794f6d6a3b
Oleksandr Slynko <alex-slynko@github.com>
2024-03-16 21:00:38 +0000

    Add instructions

--- runner.rb
+++ runner.rb
@@ -3,7 +3,10 @@
 # frozen_string_literal: true
 
 require_relative 'lib/program_processor'
-
+if len = ARGV.length != 1
+  puts "Usage: runner.rb <url>"
+  exit 1
+end
 program_processor = ProgramProcessor.new(url: ARGV[0])
 result = program_processor.process
 puts result.to_rss
